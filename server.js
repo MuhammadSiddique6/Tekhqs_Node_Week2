2ef0f88d1637f96ae54834daab8ed6e42a73caa4
MuhammadSiddique6 <muhammadsiddiquekhan5@gmail.com>
2025-04-30 12:54:00 +0500

    reset my server

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 const express = require("express");
 require("dotenv").config();
-const cors = require("cors");
-const connectDB = require("../config/db");
-const serverless = require("serverless-http");
+const connectDB = require("./config/db");
+const logger = require("./winston/index.js");
 
 const app = express();
-app.use(cors());
+
 connectDB();
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is live");
+const PORT = process.env.PORT || 3000;
+app.use("/api/users", require("./routes/user"));
+app.use("/api/verify", require("./routes/verficaltion"));
+app.use("/api/forgetpassword", require("./routes/forgetpassword.js"));
+app.use("/api/admin", require("./routes/admin.js"));
+app.listen(PORT, () => {
+  logger.info(`Server running at http://localhost:${PORT}`);
 });
-
-app.use("/api/users", require("../routes/user"));
-app.use("/api/verify", require("../routes/verificaltion")); // fixed typo in filename
-app.use("/api/forgetpassword", require("../routes/forgetpassword"));
-app.use("/api/admin", require("../routes/admin"));
-
-module.exports.handler = serverless(app);  // ✅ Important for Vercel
