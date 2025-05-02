92389943bbb7ec0e2b0680763794fab34a7851ca
MuhammadSiddique6 <muhammadsiddiquekhan5@gmail.com>
2025-05-02 11:54:06 +0500

    cors install

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ require("dotenv").config();
 const connectDB = require("./config/db");
 //const logger = require("./winston/index.js");
 const serverless = require("serverless-http");
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+
 
 connectDB();
 
@@ -13,13 +16,13 @@ app.get("/",(req,res)=>{
   res.send("running");
 });
 //const PORT = process.env.PORT || 3000;
-app.use("/api/users", require("./routes/user"));
-app.use("/api/verify", require("./routes/verficaltion"));
-app.use("/api/forgetpassword", require("./routes/forgetpassword.js"));
-app.use("/api/admin", require("./routes/admin.js"));
+app.use("/users", require("./routes/user"));
+app.use("/verify", require("./routes/verficaltion"));
+app.use("/forgetpassword", require("./routes/forgetpassword.js"));
+app.use("/admin", require("./routes/admin.js"));
 //app.listen(PORT, () => {
   //console.log(`Server running at http://localhost:${PORT}`);
 //});
 
-module.exports = app;
-module.exports.handler = serverless(app);
+
+module.exports = serverless(app);
